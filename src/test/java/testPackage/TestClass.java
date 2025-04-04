ca642b092e1d96f11f3358cfbf742abf67ec3b29
Kholoud-Rabie <kholoudrabie94@gmail.com>
2025-04-04 10:40:10 +0200

    Initialize Selenium Automation Project with Maven and basic test setup

---
+++ src/test/java/testPackage/TestClass.java
@@ -0,0 +1,23 @@
+package testPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class TestClass {
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void test() {
+        driver.get("https://duckduckgo.com/");
+        Assert.assertEquals(driver.getTitle(), "DuckDuckGo");
+    }
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
